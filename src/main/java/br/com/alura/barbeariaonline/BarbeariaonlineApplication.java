77625a47ca588ed01aef0571bb9e8d8bea4a7fb2
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2022-10-22 12:15:25 -0300

    Criação do maven

---
+++ src/main/java/br/com/alura/barbeariaonline/BarbeariaonlineApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.barbeariaonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariaonlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/barbeariaonline/BarbeariaonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.barbeariaonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
