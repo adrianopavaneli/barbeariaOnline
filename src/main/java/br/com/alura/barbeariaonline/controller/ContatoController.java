f62948bdb0ffd60e5ac29f1fd35979c15da3a48d
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2022-10-22 21:36:28 -0300

    reformulacao

--- src/main/java/br/com/alura/barbeariaonline/controller/ContatoController.java
+++ src/main/java/br/com/alura/barbeariaonline/controller/ContatoController.java
@@ -4,12 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
-	@GetMapping("/home")
-	public String home() {
-		return "home";
+public class ContatoController {
+	@GetMapping("/contato")
+	public String contato() {
+		return "contato";
 	}
-	
-	
 
 }
