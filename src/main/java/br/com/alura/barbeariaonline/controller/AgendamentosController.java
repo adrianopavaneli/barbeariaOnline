d3ed2bc707768203dd9bd11b9e844b6e4fcc02c1
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2022-10-26 21:28:55 -0300

    Agendamentos

--- src/main/java/br/com/alura/barbeariaonline/controller/AgendamentosController.java
+++ src/main/java/br/com/alura/barbeariaonline/controller/AgendamentosController.java
@@ -1,35 +1,50 @@
 package br.com.alura.barbeariaonline.controller;
 
+import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.alura.barbeariaonline.model.Agendamento;
-import br.com.alura.barbeariaonline.model.Cliente;
+import br.com.alura.barbeariaonline.model.Servico;
 import br.com.alura.barbeariaonline.repository.AgendamentoRepository;
+import br.com.alura.barbeariaonline.repository.BarbeiroRepository;
 import br.com.alura.barbeariaonline.repository.ClienteRepository;
 
 @Controller
+
 public class AgendamentosController {
 	@Autowired
 	private AgendamentoRepository agendamentoRepository;
 	@Autowired
 	private ClienteRepository clienteRepository;
+	@Autowired
+	private ServicoController servicoRepository;
+	@Autowired
+	private BarbeiroRepository barbeiroRepository;
+	
+	
+	
 	
 	
 	
 	@GetMapping("/agendamentos")
-	public String agendamentos(Model model) {
+	public String agendamentos(Model model, Principal principal) {
+	    Sort sort = Sort.by("data").ascending();
 		
-		List<Agendamento> agendamentos = agendamentoRepository.findAll();
-		
+		List<Agendamento> agendamentos = agendamentoRepository.findAllTodosAgendamentos();
+		System.out.println(agendamentos);
 		model.addAttribute("agendamentos", agendamentos);
 		
 		
 		return "agendamentos";
 	}
+	
+
 
 }
--- src/main/java/br/com/alura/barbeariaonline/repository/AgendamentoRepository.java
+++ src/main/java/br/com/alura/barbeariaonline/repository/AgendamentoRepository.java
@@ -1,6 +1,9 @@
 package br.com.alura.barbeariaonline.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.barbeariaonline.model.Agendamento;
@@ -9,4 +12,6 @@ import br.com.alura.barbeariaonline.model.Agendamento;
 @Repository
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long>{
 
+    @Query(value = "select p.id as pid, ativo, data, observacao, p.valor as pvalor, barbeiro_id, cliente_id, servico_id, c.id as cid, cep, cidade, data_nascimento, email, endereco, c.nome as cnome, rg, sexo, telefone, s.id as sid, descricao, s.valor as svalor, b.id as bid, b.nome as bnome  from agendamentos p inner join clientes c ON c.id = p.cliente_id inner join servicos s on s.id = p.servico_id inner join barbeiros b ON b.id = p.barbeiro_id WHERE ativo = TRUE", nativeQuery = true)
+    List<Agendamento> findAllTodosAgendamentos();
 }
